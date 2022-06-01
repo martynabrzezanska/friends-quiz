a006374caae8d8d4f7091951b70510f2ba462ddf
martynabrzezanska <martyna.b3110@gmail.com>
2022-06-01 22:41:35 +0000

    Displayed the current question in DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,18 +97,17 @@ const quizQuestions = [{
 
 
 let score;
-let questionEl = document.getElementsByClassName('question-bar');
+let questionEl = document.getElementById('question-bar');
 let choicesEl = document.getElementsByClassName('choice-text');
-let answerA = document.getElementById('answer-a');
-let answerB = document.getElementById('answer-b');
-let answerC = document.getElementById('answer-c');
-let answerD = document.getElementById('answer-d');
+let answerAEl = document.getElementById('answer-a');
+let answerBEl = document.getElementById('answer-b');
+let answerCEl = document.getElementById('answer-c');
+let answerDEl = document.getElementById('answer-d');
 let questionCounter = document.getElementById('question-counter');
 let nextButtonEl = document.getElementById('next-btn');
 let submitButtonEl = document.getElementById('submit-btn');
 
 let questionIndex = 0;
-let currentQuestion = quizQuestions[questionIndex];
 
 /* Hamburger menu to toggle between showing and hiding the navigation menu with a click */
 
@@ -122,14 +121,13 @@ function myFunction() {
 }
 
 function loadQuestions() {
-    let question = currentQuestion.question
-    let answerA = currentQuestion.a
-    let answerB = currentQuestion.b
-    let answerC = currentQuestion.c
-    let answerD = currentQuestion.d
-
-
-    console.log(question);
+    let currentQuestion = quizQuestions[questionIndex];
+    questionEl.textContent = currentQuestion.question
+    answerAEl.textContent = currentQuestion.a
+    answerBEl.textContent = currentQuestion.b
+    answerCEl.textContent = currentQuestion.c
+    answerDEl.textContent = currentQuestion.d
+    console.log(questionEl)
 }
 
 loadQuestions()
