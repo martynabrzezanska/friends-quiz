1033cbd0effb4cd5ce94b4c42d8f064ad270ac1e
martynabrzezanska <martyna.b3110@gmail.com>
2022-06-01 22:06:56 +0000

    Renamed the variables in script.hs for easier understading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,94 +1,93 @@
-const question = [{
+const quizQuestions = [{
         question: "Where do Rachel and Ross go on their first date?",
-        answers: {
-            option1: "The cinema",
-            option2: "A planetarium",
-            option3: "Central Park",
-            option4: "The Empire State Building"
-        },
-        correctAnswer: "2"
+
+        a: "The cinema",
+        b: "A planetarium",
+        c: "Central Park",
+        d: "The Empire State Building",
+        correctAnswer: "2",
     },
 
     {
         question: "Monica organizes her towels into several categories. But how many?",
-        option1: "4",
-        option2: "6",
-        option3: "9",
-        option4: "11",
+        a: "4",
+        b: "6",
+        c: "9",
+        d: "11",
         correctAnswer: "4",
     },
 
     {
         question: "What are Monica and Ross' parents called?",
-        option1: "Jack and Judy",
-        option2: "Jack and Jill",
-        option3: "Peter and Margaret",
-        option4: "Gerry and Mary",
+        a: "Jack and Judy",
+        b: "Jack and Jill",
+        c: "Peter and Margaret",
+        d: "Gerry and Mary",
         correctAnswer: "4",
     },
 
     {
         question: "Chandler and Joey had two pets. What were they called?",
-        option1: "Don and Camilla",
-        option2: "Brian and Larry",
-        option3: "Walter and Sergio",
-        option4: "Chick Jr. and Duck Jr.",
+        a: "Don and Camilla",
+        b: "Brian and Larry",
+        c: "Walter and Sergio",
+        d: "Chick Jr. and Duck Jr.",
         correctAnswer: "4",
     },
 
     {
         question: "What is the name of Joey’s acting agent?",
-        option1: "Andrea",
-        option2: "Estelle",
-        option3: "Martha",
-        option4: "Carole",
+        a: "Andrea",
+        c: "Estelle",
+        c: "Martha",
+        d: "Carole",
         correctAnswer: "2",
     },
 
 
     {
         question: "Which job did Rachel’s fiancé Barry Farber do?",
-        option1: "Lawyer",
-        option2: "Plumber",
-        option3: "Orthodontist",
-        option4: "Journalist",
+        a: "Lawyer",
+        b: "Plumber",
+        c: "Orthodontist",
+        d: "Journalist",
         correctAnswer: "3",
     },
 
     {
         question: "Ross is allergic to which kind of fruit?",
-        option1: "Lemon",
-        option2: "Apple",
-        option3: "Kiwi",
-        option4: "Pomegranate",
+        a: "Lemon",
+        b: "Apple",
+        c: "Kiwi",
+        d: "Pomegranate",
         correctAnswer: "3",
     },
 
     {
         question: "Phoebe dated a scientist called David. In which Belarus city did he work?",
-        option1: "Minsk",
-        option2: "Grodno",
-        option3: "Mogilev",
-        option4: "Pinsk",
+        a: "Minsk",
+        b: "Grodno",
+        c: "Mogilev",
+        d: "Pinsk",
         correctAnswer: "1",
     },
 
     {
         question: "Ross had a son called Ben. What was Ben's first word?",
-        option1: "Dinosaur",
-        option2: "Coffee",
-        option3: "Daddy",
-        option4: "Hi",
+        a: "Dinosaur",
+        b: "Coffee",
+        c: "Daddy",
+        d: "Hi",
         correctAnswer: "4",
     },
 
 
     {
         question: "Monica dated a millionaire for a while. What was his name?",
-        option1: "Paul Becker",
-        option2: "Jake Becker",
-        option3: "Pete Becker",
-        option4: "Boris Becker",
+        a: "Paul Becker",
+        b: "Jake Becker",
+        c: "Pete Becker",
+        d: "Boris Becker",
         correctAnswer: "3",
     },
 
@@ -98,18 +97,17 @@ const question = [{
 document.addEventListener('DOMContentLoaded', () => {
 
     let score;
-    let correctAnswer;
-    let acceptAnswers;
-    let maxQuestions = 10;
     let currentQuestionIndex = 0;
-    const question = document.getElementsByClassName('question-bar');
-    let choices = document.getElementsByClassName('choice-text');
+    let questionEl = document.getElementsByClassName('question-bar');
+    let choicesEl = document.getElementsByClassName('choice-text');
+    let answerA = document.getElementById('answer-a');
+    let answerB = document.getElementById('answer-b');
+    let answerC = document.getElementById('answer-c');
+    let answerD = document.getElementById('answer-d');
     let questionCounter = document.getElementById('question-counter');
-    let nextButton = document.getElementsByClassName('next-btn')[0];
-    let submitButton = document.getElementsByClassName('submit-btn')[0];
+    let nextButtonEl = document.getElementById('next-btn');
+    let submitButtonEl = document.getElementById('submit-btn');
 
-    nextButton.addEventListener('click', nextQuestion);
-    restartButton.addEventListener('click', restartQuiz);
 })
 
 /* Hamburger menu to toggle between showing and hiding the navigation menu with a click */
@@ -121,4 +119,8 @@ function myFunction() {
     } else {
         x.style.display = "block";
     }
+}
+
+function loadQuestions() {
+    answerA = quizQuestions.
 }
